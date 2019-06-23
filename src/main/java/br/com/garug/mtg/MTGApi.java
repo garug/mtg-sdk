29af5637c98b883295511ee2a4823a841d712bda
garug <allefff@gmail.com>
2019-06-23 17:43:19 -0300

    Added findAll and best practices applied

--- src/main/java/br/com/garug/mtg/MTGApi.java
+++ src/main/java/br/com/garug/mtg/MTGApi.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,49 +25,55 @@ import br.com.garug.mtg.implementations.SetMTGApi;
 
 public interface MTGApi {
 	Logger LOGGER = LoggerFactory.getLogger(MTGApi.class);
-    String API_URL = "https://api.magicthegathering.io/v1";
-    CardMTGApi card = new CardMTGApi() {};
-    SetMTGApi set = new SetMTGApi() {};
+	String API_URL = "https://api.magicthegathering.io/v1";
+	CardMTGApi card = new CardMTGApi() {	};
+	SetMTGApi set = new SetMTGApi() {	};
 
-    public static List<? extends MTGEntity> findAll() {
-    	// TODO implements
-		return null;
+	public static List<? extends MTGEntity> findAll(MTGEntityEnum type) {
+		JsonNode node = getJson(API_URL + "/" + type.getMultiple());
+		ObjectMapper mapper = new ObjectMapper();
+		return dealWithNode(node, type.getMultiple(), mapper.getTypeFactory().constructCollectionType(List.class, type.getJavaType()));
 	}
-    
-    public static MTGEntity findOne(String id, MTGEntityEnum type) {
-    	JsonNode node = getJson(API_URL + "/" + type.getMultiple() + "/" + id);
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-        	return mapper.readValue(node.get(type.getOne()).toString(), type.getJavaType());
-        } catch(JsonParseException | JsonMappingException e) {
-            LOGGER.error("Some error in deserialize object", e);
-        } catch (IOException e) {
-        	LOGGER.error("Some error in deserialize specific " + type.getOne(), e);
-        }
-        throw new IllegalArgumentException("Cannot possible retrieve a entity with actual data" );
-    }
-    
-    static JsonNode getJson(String url) {
-    	// Get string represent JSON
-        String json = null;
-        try(CloseableHttpClient httpClient = HttpClients.createDefault()) {
-        	HttpGet request = new HttpGet(url);
-        	HttpResponse response = httpClient.execute(request);
-        	json = EntityUtils.toString(response.getEntity());
-        } catch (ParseException e) {
-        	LOGGER.error("Some error in parse object response: " + url, e);
-        } catch (IOException e) {
-        	LOGGER.error("Some error in request", e);
-        }
-        
-        // Construct JsonNode
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            return mapper.readValue(json, JsonNode.class);
-        } catch (IOException e) {
-            // TODO implement catch
-        }
-        
-        throw new IllegalArgumentException("Can't return a JsonNode for request: " + url);
-    }
+
+	public static MTGEntity findOne(String id, MTGEntityEnum type) {
+		JsonNode node = getJson(API_URL + "/" + type.getMultiple() + "/" + id);
+		return dealWithNode(node, type.getOne(), type.getJavaType());
+	}
+
+	static JsonNode getJson(String url) {
+		// Get string represent JSON
+		String json = null;
+		try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+			HttpGet request = new HttpGet(url);
+			HttpResponse response = httpClient.execute(request);
+			json = EntityUtils.toString(response.getEntity());
+		} catch (ParseException e) {
+			LOGGER.error("Some error in parse object response: " + url, e);
+		} catch (IOException e) {
+			LOGGER.error("Some error in request", e);
+		}
+
+		// Construct JsonNode
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(json, JsonNode.class);
+		} catch (IOException e) {
+			// TODO implement catch
+		}
+
+		throw new IllegalArgumentException("Can't return a JsonNode for request: " + url);
+	}
+
+	static <T> T dealWithNode(JsonNode node, String key, JavaType type) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(node.get(key).toString(), type);
+		} catch (JsonParseException | JsonMappingException e) {
+			LOGGER.error("Some error in deserialize object", e);
+		} catch (IOException e) {
+			LOGGER.error("Some error in deserialize specific(s) " + key, e);
+		}
+		throw new IllegalArgumentException("Cannot possible retrieve a entity with actual data");
+		
+	}
 }
