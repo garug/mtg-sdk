9d2339b7c5780d5ac5dbdf1ddbb1a84754f863ee
garug <allefff@gmail.com>
2019-06-23 17:43:40 -0300

    FindAll implemented 29af5637c98b883295511ee2a4823a841d712bda

--- src/main/java/br/com/garug/mtg/implementations/CardMTGApi.java
+++ src/main/java/br/com/garug/mtg/implementations/CardMTGApi.java
@@ -1,14 +1,21 @@
 package br.com.garug.mtg.implementations;
 
+import java.util.List;
+
 import br.com.garug.mtg.MTGApi;
 import br.com.garug.mtg.entities.enums.MTGEntityEnum;
 import br.com.garug.mtg.entities.implementations.Card;
 
 public interface CardMTGApi extends MTGApi {
-	
+
 	MTGEntityEnum type = MTGEntityEnum.CARD;
-	
+
 	public default Card findOne(String id) {
 		return (Card) MTGApi.findOne(id, type);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public default List<Card> findAll() {
+		return (List<Card>) MTGApi.findAll(type);
 	}
 }
--- src/main/java/br/com/garug/mtg/implementations/SetMTGApi.java
+++ src/main/java/br/com/garug/mtg/implementations/SetMTGApi.java
@@ -1,12 +1,21 @@
 package br.com.garug.mtg.implementations;
 
+import java.util.List;
+
 import br.com.garug.mtg.MTGApi;
 import br.com.garug.mtg.entities.enums.MTGEntityEnum;
 import br.com.garug.mtg.entities.implementations.Set;
 
 public interface SetMTGApi extends MTGApi {
-MTGEntityEnum type = MTGEntityEnum.SET;
+
+	MTGEntityEnum type = MTGEntityEnum.SET;
+
 	public default Set findOne(String id) {
 		return (Set) MTGApi.findOne(id, type);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public default List<Set> findAll() {
+		return (List<Set>) MTGApi.findAll(type);
 	}
 }
